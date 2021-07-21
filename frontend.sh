e2e0ae0b8c8168b35ed616db96d812849a6f92a6
saikiran.ask007@gmail.com <$@!kiranGIT007>
2021-07-20 20:52:34 -0400

    Completing the Frontend file

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,20 @@ printf  "Installing Nginx .... "
 yum install nginx -y &>>$log
 status_check $?
 
+printf "Downloading the frontend content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$log
+status_check $?
+
+printf "Removing default content fro Nginx"
+cd /usr/share/nginx/html &>>$log && rm -rf * &>>$log
+
+printf " Extracting the content "
+unzip /tmp/frontend.zip &>>$log && mv frontend-main/* . &>>$log && mv static/* . &>>$log && rm -rf frontend-master static &>>$log
+
+printf " Updating Roboshop Config by moving it to Nginx default d filr"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$log
+
+
 printf  "Enabling  Nginx .... "
 systemctl enable nginx &>>$log
 status_check $?

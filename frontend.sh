4ea47f1d49d2057b566dcdefc7878392d6a111bb
saikiran.ask007@gmail.com <$@!kiranGIT007>
2021-07-20 20:31:22 -0400

    Testing Frontend and common files - #4

--- frontend.sh
+++ frontend.sh
@@ -2,15 +2,15 @@
 
 source common.sh
 
-echo -n -e "\e[32mInstalling Nginx ....\e[0m \t"
+echo  "Installing Nginx .... "
 yum install nginx -y &>>$log
 status_check $?
 
-echo  -n -e "\e[32mEnabling  Nginx ....\e[0m "
+echo  "Enabling  Nginx .... "
 systemctl enable nginx &>>$log
 status_check $?
 
-echo  -n -e "\e[32mStarting Nginx ....\e[0m "
+echo  "Starting Nginx .... "
 systemctl start nginx &>>$log
 status_check $?
 

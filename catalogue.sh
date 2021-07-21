ecd433283451fb63e8d118a3e35dd9cf16ed5f44
saikiran.ask007@gmail.com <$@!kiranGIT007>
2021-07-20 23:24:36 -0400

    Creating Catalogue-#1

---
+++ catalogue.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+printf  "Installing NodeJS"
+yum install nodejs make gcc-c++ -y &>>$log
+status_check $?
+
+
+printf  "Adding roboshop daemon user "
+useradd roboshop &>>$log
+status_check $?
+# yum install nodejs make gcc-c++ -y
+# useradd roboshop
+#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#$ cd /home/roboshop
+#$ unzip /tmp/catalogue.zip
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install

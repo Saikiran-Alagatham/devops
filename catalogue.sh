9a3f8a24ae4a3631fff90c4b63b074581852de50
saikiran.ask007@gmail.com <$@!kiranGIT007>
2021-07-21 18:58:33 -0400

    Completing the catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -8,14 +8,32 @@ status_check $?
 
 
 printf  "Adding roboshop daemon user "
-useradd roboshop &>>$log
+id roboshop &>>$log
+if [ $? -n 0 ] ; then
+  useradd roboshop &>>$log
+fi
 status_check $?
 
-# yum install nodejs make gcc-c++ -y
-# useradd roboshop
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
+printf "Downloading the Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+status_check $?
+
+printf " Extracting downloaded Code"
+cd /home/roboshop && unzip /tmp/catalogue.zip && rm -rf catalogue && mv catalogue-main catalogue &>>$log
+status_check $?
+
+printf "Installing NodeJs dependencies"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$log
+status_check $?
+
+printf "Fixing the permissions"
+chdown roboshop:roboshop /home/roboshop -R &>>$log
+status_check $?
+
+printf " Setting up SystemD file"
+sed - i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$log
+status_check $?
+
+printf "Starting the catalogue Service"
+systemctl daemon-reload >>$log && systemctl start catalogue>>$log && systemctl enable catalogue>>$log
+status_check $?
